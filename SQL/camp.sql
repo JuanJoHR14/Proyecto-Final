0736697ec90ad7e3030cffa0cfb849381e922f54
JuanJoHR14 <158634436+JuanJoHR14@users.noreply.github.com>
2025-10-23 08:02:17 -0500

    Add files via upload

---
+++ SQL/camp.sql
@@ -0,0 +1,144 @@
+CREATE DATABASE  IF NOT EXISTS `camp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `camp`;
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: localhost    Database: camp
+-- ------------------------------------------------------
+-- Server version	8.0.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `carrito`
+--
+
+DROP TABLE IF EXISTS `carrito`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `carrito` (
+  `id_carr` int NOT NULL AUTO_INCREMENT,
+  `producto` varchar(10) NOT NULL,
+  PRIMARY KEY (`id_carr`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `carrito`
+--
+
+LOCK TABLES `carrito` WRITE;
+/*!40000 ALTER TABLE `carrito` DISABLE KEYS */;
+/*!40000 ALTER TABLE `carrito` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `factura`
+--
+
+DROP TABLE IF EXISTS `factura`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `factura` (
+  `id_fac` int NOT NULL AUTO_INCREMENT,
+  `precio` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_fac`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `factura`
+--
+
+LOCK TABLES `factura` WRITE;
+/*!40000 ALTER TABLE `factura` DISABLE KEYS */;
+/*!40000 ALTER TABLE `factura` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inventario`
+--
+
+DROP TABLE IF EXISTS `inventario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inventario` (
+  `id_inv` int NOT NULL AUTO_INCREMENT,
+  `producto` varchar(10) NOT NULL,
+  `cant_pro` varchar(15) NOT NULL,
+  `id_usu` int NOT NULL,
+  `id_rol` varchar(8) NOT NULL,
+  PRIMARY KEY (`id_inv`),
+  KEY `id_usu_idx` (`id_usu`),
+  KEY `id_rol_idx` (`id_rol`),
+  CONSTRAINT `id_usu` FOREIGN KEY (`id_usu`) REFERENCES `usuario_b` (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inventario`
+--
+
+LOCK TABLES `inventario` WRITE;
+/*!40000 ALTER TABLE `inventario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `inventario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario_b`
+--
+
+DROP TABLE IF EXISTS `usuario_b`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario_b` (
+  `id_usuario` int NOT NULL,
+  `rol` enum('Vendedor','Comprador') NOT NULL,
+  `nom` varchar(16) NOT NULL,
+  `apll` varchar(18) NOT NULL,
+  `fec_nac` datetime NOT NULL,
+  `genero` varchar(9) NOT NULL,
+  `pais` varchar(10) DEFAULT NULL,
+  `ciudad` varchar(10) NOT NULL,
+  `id_fac` int NOT NULL,
+  PRIMARY KEY (`id_usuario`,`rol`),
+  KEY `id_fac_idx` (`id_fac`),
+  CONSTRAINT `id_fac` FOREIGN KEY (`id_fac`) REFERENCES `factura` (`id_fac`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario_b`
+--
+
+LOCK TABLES `usuario_b` WRITE;
+/*!40000 ALTER TABLE `usuario_b` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario_b` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'camp'
+--
+
+--
+-- Dumping routines for database 'camp'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-09-15 22:28:04
